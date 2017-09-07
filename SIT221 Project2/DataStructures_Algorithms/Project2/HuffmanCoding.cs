b032773a972b867f6d377fe8a3b248672f075836
Blacq_Shadow <rudrak1996@gmail.com>
2017-09-07 15:34:15 +1000

    initial commit

---
+++ SIT221 Project2/DataStructures_Algorithms/Project2/HuffmanCoding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures_Algorithms.Project1;
+
+namespace DataStructures_Algorithms.Project2
+{
+    // Material
+    // https://en.wikipedia.org/wiki/Huffman_coding
+    // https://www.siggraph.org/education/materials/HyperGraph/video/mpeg/mpegfaq/huffman_tutorial.html
+    // http://www.geeksforgeeks.org/greedy-algorithms-set-3-huffman-coding/
+
+    public class HuffmanCoding
+    {
+        public Vector<string> Encode(Vector<char> input)
+        {
+            return null;
+        }
+
+        public Vector<char> Decode(Vector<string> input)
+        {
+            return null;
+        }
+    }
+}
